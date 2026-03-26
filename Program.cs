d340d68d7c1ff771f0adb6a447182492e3787f91
uvscube <saro@uvscube.com>
2026-03-26 17:11:45 +0530

    Initial Checkin

---
+++ Program.cs
@@ -0,0 +1,46 @@
+
+using Microsoft.OpenApi;
+using System.Reflection;
+
+namespace SampleWebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = Assembly.GetExecutingAssembly().GetName().Name,
+                    Version = "v1"
+                });
+            });
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
